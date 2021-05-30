b6dca69d2ad68136c1eb6782e4551b615241ca39
AKSHAYAG1999 <52597731+AKSHAYAG1999@users.noreply.github.com>
2021-05-30 10:53:12 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,22 +38,6 @@ int main(void) {
     }
   }
 }
-float mc_pi(int count)
-{
-	float circount=0;
-	for(int i=0;i<count;i++)
-	{
-		
-		float x = frandom();
-		float y = frandom();
-		if(sqrt(x*x+y*y)<1)
-		{
-			circount++;
-		}
-	}
-	float area = ((4*circount)/count);
-	return area;
-}
 
 
 
